68314af423177a69a969d162517a96b6e67e57c9
rinmon <rinmon@cn7.jp>
2023-05-15 08:47:22 +0900

    2023/05/14の最終版

--- output_last_message.py
+++ output_last_message.py
@@ -3,6 +3,7 @@ import json
 import requests
 import sys
 import datetime
+import pandas as pd
 from urllib.parse import quote
 
 # 設定ファイルを読み込みます。
@@ -19,26 +20,25 @@ token = config["token"]
 headers = {"Authorization": f"Bearer {token}"}
 
 # 出力ファイルを作成します。
-output_file_path = os.path.join(config["excel_dir"], "output_last_message.txt")
-
+output_file_path = os.path.join(config["excel_dir"], "output_last_message.xlsx")
 
 def get_all_teams():
     response = requests.get(f"{url}/api/v4/teams", headers=headers)
     response.raise_for_status()
     return response.json()
 
-
 def get_channels_for_team(team_id):
-    public_channels_response = requests.get(
-        f"{url}/api/v4/teams/{team_id}/channels", headers=headers
-    )
+    public_channels_response = requests.get(f"{url}/api/v4/teams/{team_id}/channels", headers=headers)
     public_channels_response.raise_for_status()
-    private_channels_response = requests.get(
-        f"{url}/api/v4/users/me/teams/{team_id}/channels", headers=headers
-    )
+    private_channels_response = requests.get(f"{url}/api/v4/users/me/teams/{team_id}/channels", headers=headers)
     private_channels_response.raise_for_status()
-    return public_channels_response.json() + private_channels_response.json()
+    channels = public_channels_response.json() + private_channels_response.json()
 
+    # Remove possible duplicates and empty channel names
+    channels = [channel for channel in channels if channel["display_name"].strip() != ""]
+    channels = list({v['id']:v for v in channels}.values())
+    
+    return channels
 
 def get_last_message_info(channel_id):
     response = requests.get(
@@ -64,17 +64,13 @@ def get_last_message_info(channel_id):
 
     return last_post_time
 
+data = []
 
-with open(output_file_path, "w", encoding="utf-8") as output_file:
-    for team in get_all_teams():
-        output_file.write(f"Team: {team['display_name']} - Team ID: {team['id']}\n")
-        for channel in get_channels_for_team(team["id"]):
-            last_message_time = get_last_message_info(channel["id"])
-            channel_type = "Pub" if channel["type"] == "O" else "Pvt"
-            if last_message_time is not None:
-                output_file.write(
-                    f"{channel_type} Channel: {channel['display_name']} - Channel ID: {channel['id']} - Last message time: {last_message_time}\n"
-                )
-        output_file.write("\n")
-
-print(f"テキストファイルに処理内容を出力しました: {output_file_path}")
+for team in get_all_teams():
+    for channel in get_channels_for_team(team["id"]):
+        last_message_time = get_last_message_info(channel["id"])
+        channel_type = "Public" if channel["type"] == "O" else "Private"
+        if last_message_time is not None:
+            data.append({
+                'Team': team['display_name'],
+               
